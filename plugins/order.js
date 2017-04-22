1c48e9d58a4867bf9fe8eaeb6c3c45efd3b88621
Phillip Gray <phillip@bignerdranch.com>
2017-04-22 11:22:04 -0400

    Finished step 2

--- plugins/order.js
+++ plugins/order.js
@@ -1,7 +1,20 @@
+const pizzapi = require('dominos')
+
 module.exports = {
   init: (controller) => {
     controller.hears([/i want a pizza/, /i want pizza/], ['direct_message', 'direct_mention', 'ambient'], (bot, message) => {
-      bot.reply(message, "Sup! Looking for nearby stores...")
+      bot.reply(message, 'Sup! Looking for nearby stores...')
+      pizzapi.Util.findNearbyStores('11 Times Square, New York, NY 10036', 'Delivery', (storeData) => {
+        let storeList = storeData.result.Stores
+        let filteredStoreList = storeList.filter((store) => {
+          return store.IsOpen && store.IsOnlineCapable && store.IsOnlineNow
+        })
+        let shortList = filteredStoreList.reduce((newString, store) => {
+          newString += `ID: ${store.StoreID}\r\nAddress: ${store.AddressDescription}\r\n\r\n`
+          return newString
+        }, '')
+        bot.reply(message, shortList)
+      })
     })
   },
   help: {
